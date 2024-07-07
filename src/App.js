3326cd8a30997061388a9c75eb614e169965bb8b
lordbeerus55 <kshitijp099@gmail.com>
2024-07-07 22:41:36 +0530

    pushing everything to git

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ListEmployeeComponent from "../src/components/ListEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmployeeComponent from "./components/EmployeeComponent";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<ListEmployeeComponent />}></Route>
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+          <Route path = "/add-employee" element={<EmployeeComponent/>}></Route>
+          <Route path='/edit-employee/:id' element = { <EmployeeComponent /> }></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className='navbar navbar-dark bg-dark'>
+            <a class="navbar-brand" > Employee Management System</a> </nav>
+      </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
